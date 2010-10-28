4616be7287b37f3b6aa0c81e753557e4dc2f8242
wira <wira@home>
2010-10-27 20:47:07 -0500

    Changed object code array to long from int, since Java doesn't support unsigned types. Now we can have values that use the 32th bit without Java complaining. Added PLPAsmFormatter for PLP format output and other goodies, will work on it tomorrow.

---
+++ reference/plp-2.0/sw/PLPTool/src/plptool/PLPAsmFormatter.java
@@ -0,0 +1,13 @@
+/*
+ * Utility class to package / display object codes produces by PLPAsm
+ */
+
+package plptool;
+
+/**
+ *
+ * @author wira
+ */
+public class PLPAsmFormatter {
+
+}
---
+++ reference/plp-2.0/sw/PLPTool/testcase/test.asm
@@ -0,0 +1,13 @@
+          
+# Test assembly
+
+	.org 0xfff
+label1:
+	.word 0xBEEFCAFE
+	addu $2, $1, $3  #  sdfsdfsdf
+
+	.org 0xaaa
+	lui  $3,50    #dfdsfsdf
+	nop
+	nop
+	addiu $2,  $3,   0b100100   
