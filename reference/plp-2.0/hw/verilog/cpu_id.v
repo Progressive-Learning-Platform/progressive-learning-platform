7b4549a005733c4538bb3e65e0063dc72a953169
fritz <fritz@localhost>
2010-12-04 16:07:08 -0600

    updated ID stage with hazard logic

--- reference/plp-2.0/hw/verilog/cpu_id.v
+++ reference/plp-2.0/hw/verilog/cpu_id.v
@@ -6,7 +6,7 @@ instruction decode phase
 
 */
 
-module cpu_id(rst, clk, if_pc, if_inst, wb_rfw, wb_rf_waddr, wb_rf_wdata, p_rfa, p_rfb, p_rfbse, p_shamt, p_func, p_rf_waddr, p_c_rfw, p_c_wbsource, p_c_drw, p_c_alucontrol, p_c_j, p_c_b, p_c_jjr, p_jaddr, p_pc);
+module cpu_id(rst, clk, if_pc, if_inst, wb_rfw, wb_rf_waddr, wb_rf_wdata, p_rfa, p_rfb, p_se, p_shamt, p_func, p_rf_waddr, p_c_rfw, p_c_wbsource, p_c_drw, p_c_alucontrol, p_c_j, p_c_b, p_c_jjr, p_jaddr, p_pc, p_c_rfbse, p_rs, p_rt);
 	input 		rst, clk;
 	input	[31:0]	if_pc;
 	input	[31:0]	if_inst;
@@ -15,7 +15,7 @@ module cpu_id(rst, clk, if_pc, if_inst, wb_rfw, wb_rf_waddr, wb_rf_wdata, p_rfa,
 	input   [31:0]  wb_rf_wdata;
 	output reg [31:0] p_rfa;
 	output reg [31:0] p_rfb;
-	output reg [31:0] p_rfbse;
+	output reg [31:0] p_se;
 	output reg [4:0]  p_shamt;
 	output reg [5:0]  p_func;
 	output reg [4:0]  p_rf_waddr;
@@ -28,6 +28,9 @@ module cpu_id(rst, clk, if_pc, if_inst, wb_rfw, wb_rf_waddr, wb_rf_wdata, p_rfa,
 	output reg 	  p_c_jjr;
 	output reg [25:0] p_jaddr;
 	output reg [31:0] p_pc;
+	output reg	  p_c_rfbse;
+	output reg [4:0]  p_rs;
+	output reg [4:0]  p_rt;
 
 	reg [31:0] rf [31:1];
 
@@ -42,18 +45,21 @@ module cpu_id(rst, clk, if_pc, if_inst, wb_rfw, wb_rf_waddr, wb_rf_wdata, p_rfa,
 	wire [31:0] rfa = rf_rs == 0 ? 0 : rf[rf_rs];
 	wire [31:0] rfb = rf_rt == 0 ? 0 : rf[rf_rt];
 
+	/* hazard logic */
+	wire stall = ((p_c_alucontrol == 6'h23) & ((p_rt == rf_rs) | (p_rt == rf_rt)) & (p_rt != 0));
 
 	/* control logic */
 	wire c_rfw = ( 
 		opcode != 6'h04 && 
 		opcode != 6'h05 && 
 		opcode != 6'h2b && 
-		opcode != 6'h02); /* secret bug, jump register asserts write enable but the assembler sets rd = 0 */
+		opcode != 6'h02 && 
+		!stall); /* secret bug, jump register asserts write enable but the assembler sets rd = 0 */
 	wire [1:0] c_wbsource = 
 		(opcode == 6'h23) ? 2'h1 :
 		(opcode == 6'h03) ? 2'h2 :
 		(opcode == 6'h00 && func == 6'h09) ? 2'h2 : 0; 
-	wire c_drw = opcode == 6'h2b ? 1 : 0;
+	wire c_drw = (opcode == 6'h2b && !stall) ? 1 : 0;
 	wire [5:0] c_alucontrol = opcode;
 	wire c_se = (opcode == 6'h0c || opcode == 6'h0d) ? 0 : 1;
 	wire c_rfbse = opcode == 6'h00 ? 0 : 1;
@@ -72,21 +78,19 @@ module cpu_id(rst, clk, if_pc, if_inst, wb_rfw, wb_rf_waddr, wb_rf_wdata, p_rfa,
 		(c_rd_rt_31 == 2'b00) ? rf_rd :
 		(c_rd_rt_31 == 2'b01) ? rf_rt :
 		(c_rd_rt_31 == 2'b10) ? 5'b11111 : rf_rd;
-	wire [31:0] rfbse = c_rfbse ? se : rf_rt;
 
-	assign c_j = 
+	wire c_j = 
 		(opcode == 6'h02) || 
 		(opcode == 6'h03) || 
 		(opcode == 6'h00 && func == 6'h08) || 
 		(opcode == 6'h00 && func == 6'h09);
 
-	assign c_b = (opcode == 6'h04) || (opcode == 6'h05);
+	wire c_b = (opcode == 6'h04) || (opcode == 6'h05);
 
 	always @(posedge clk) begin
 		if (rst) begin		
 			p_rfa <= 0;
 			p_rfb <= 0;
-			p_rfbse <= 0;
 			p_shamt <= 0;
 			p_func <= 0;
 			p_rf_waddr <= 0;
@@ -99,10 +103,13 @@ module cpu_id(rst, clk, if_pc, if_inst, wb_rfw, wb_rf_waddr, wb_rf_wdata, p_rfa,
 			p_c_jjr <= 0;
 			p_jaddr <= 0;
 			p_pc <= 0;
+			p_c_rfbse <= 0;
+			p_rs <= 0;
+			p_rt <= 0;
+			p_se <= 0;
 		end else begin
 			p_rfa <= rfa;
 			p_rfb <= rfb;
-			p_rfbse <= rfbse;
 			p_shamt <= shamt;
 			p_func <= func;
 			p_rf_waddr <= rd_rt_31;
@@ -115,6 +122,10 @@ module cpu_id(rst, clk, if_pc, if_inst, wb_rfw, wb_rf_waddr, wb_rf_wdata, p_rfa,
 			p_c_jjr <= c_jjr;
 			p_jaddr <= jaddr;
 			p_pc <= if_pc;
+			p_c_rfbse <= c_rfbse;
+			p_rs <= rf_rs;
+			p_rt <= rf_rt;
+			p_se <= se;
 		end
 	
 		/* debug statements, not synthesized by Xilinx */
