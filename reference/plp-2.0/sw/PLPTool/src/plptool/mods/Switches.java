99857d8c98cec45ce96f78c9e7f77fe66cb9be80
wira <wira@Alaei>
2010-12-08 18:14:07 -0600

    PLPTool: Switches I/O files checked in. Removing ./build from repo for cleanliness.

---
+++ reference/plp-2.0/sw/PLPTool/src/plptool/mods/Switches.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package plptool.mods;
+import plptool.Constants;
+import plptool.PLPSimBusModule;
+import plptool.PLPMsg;
+
+/**
+ *
+ * @author wira
+ */
+public class Switches extends PLPSimBusModule {
+
+    public Switches(long addr) {
+        super(addr, addr, true);
+    }
+
+    public int eval() {
+        // No need to eval every cycle
+        return Constants.PLP_OK;
+    }
+
+    public int gui_eval(Object x) {
+        return Constants.PLP_OK;
+    }
+
+    public String introduce() {
+        return "Switches";
+    }
+
+    @Override
+    public int write(long addr, Object data, boolean isInstr) {
+        return PLPMsg.E("write(): Write to read only register.",
+                        Constants.PLP_SIM_WRITE_TO_READONLY_MODULE, this);
+    }
+
+    @Override
+    public String toString() {
+        return "Switches";
+    }
+}
