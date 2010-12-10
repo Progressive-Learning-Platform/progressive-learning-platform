5d605cfec865c4a5529ff816105b6aed3e79097c
fritz <fritz@localhost>
2010-12-10 10:39:45 -0600

    added vga module

--- reference/plp-2.0/hw/verilog/top.v
+++ reference/plp-2.0/hw/verilog/top.v
@@ -6,7 +6,7 @@ top level module for mips design
 2.6.2010
 */
 
-module top(clk,leds,rst,txd,rxd,switches,sseg_an,sseg_display);
+module top(clk,leds,rst,txd,rxd,switches,sseg_an,sseg_display,rgb,hs,vs);
 	input clk;
 	output [7:0] leds;
 	input rst,rxd;
@@ -14,12 +14,14 @@ module top(clk,leds,rst,txd,rxd,switches,sseg_an,sseg_display);
 	input [7:0] switches;
 	output [3:0] sseg_an;
 	output [7:0] sseg_display;
+	output [7:0] rgb;
+	output hs, vs;
 	
 	wire [31:0] daddr, dout, din, iaddr, iin;
 	wire drw;
 
 	cpu cpu_t(rst, clk, daddr, dout, din, drw, iaddr, iin);
-        arbiter arbiter_t(rst, clk, daddr, dout, din, drw, iaddr, iin, leds, txd, rxd, switches, sseg_an, sseg_display);
+        arbiter arbiter_t(rst, clk, daddr, dout, din, drw, iaddr, iin, leds, txd, rxd, switches, sseg_an, sseg_display, rgb, hs, vs);
 endmodule
 	
 
