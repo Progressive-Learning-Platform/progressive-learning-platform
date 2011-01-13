3b963f3dc4ee7012c3b118bde63f0503f7961c80
wira <wira@Tiberius>
2011-01-13 16:58:41 -0600

    merged

--- reference/plp-2.1/hw/verilog/top.v
+++ reference/plp-2.1/hw/verilog/top.v
@@ -44,9 +44,9 @@ module top(clk,leds,rst,txd,rxd,switches,sseg_an,sseg_display, mod_sram_clk, mod
 	wire drw;
 	wire cpu_c, arbiter_c, cpu_stall;
 
-	clock c_t(clk, cpu_stall, arbiter_c, cpu_c);
-	cpu cpu_t(rst, cpu_c, daddr, dout, din, drw, iaddr, iin);
-        arbiter arbiter_t(rst, arbiter_c, daddr, dout, din, drw, iaddr, iin, leds, txd, rxd, switches, sseg_an, sseg_display, cpu_stall, mod_sram_clk, mod_sram_adv, mod_sram_cre, mod_sram_ce, mod_sram_oe, mod_sram_we, mod_sram_lb, mod_sram_ub, mod_sram_data, mod_sram_addr, mod_vga_rgb, mod_vga_hs, mod_vga_vs, mod_gpio_gpio);
+	clock c_t(clk, c);
+	cpu cpu_t(rst, c, cpu_stall, daddr, dout, din, drw, iaddr, iin);
+        arbiter arbiter_t(rst, c, daddr, dout, din, drw, iaddr, iin, leds, txd, rxd, switches, sseg_an, sseg_display, cpu_stall, mod_sram_clk, mod_sram_adv, mod_sram_cre, mod_sram_ce, mod_sram_oe, mod_sram_we, mod_sram_lb, mod_sram_ub, mod_sram_data, mod_sram_addr, mod_vga_rgb, mod_vga_hs, mod_vga_vs, mod_gpio_gpio);
 endmodule
 	
 
