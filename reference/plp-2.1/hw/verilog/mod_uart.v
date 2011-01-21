c71a885cf3cad6432cf24ebc1819eea74a19267b
fritz <fritz@localhost>
2011-01-21 09:08:26 -0600

    updated ucf file, uart is now 57600 bps, up from 9600

--- reference/plp-2.1/hw/verilog/mod_uart.v
+++ reference/plp-2.1/hw/verilog/mod_uart.v
@@ -97,7 +97,7 @@ module uart_baud_generator(clk,baud,baud16,rst);
 	output baud;
 	output baud16;
 	
-	parameter b_rate = 9600;
+	parameter b_rate = 57600;
 	parameter c_rate = 25000000; /* 25 Mhz */
 	parameter divider = c_rate / b_rate;
 	parameter divider16 = c_rate / (16 * b_rate); /* 16 times faster! */
