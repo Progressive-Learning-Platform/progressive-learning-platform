50ba24575dfece8ca9cc3b0b87b8f39708edbcb2
wira <wira@Alaei>
2011-03-03 07:37:50 -0600

    plptool uart frame update

--- reference/plp-2.2/sw/PLPTool/src/plptool/mods/UARTFrame.java
+++ reference/plp-2.2/sw/PLPTool/src/plptool/mods/UARTFrame.java
@@ -40,6 +40,10 @@ public class UARTFrame extends javax.swing.JInternalFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         txtUART = new javax.swing.JTextArea();
+        btnClear = new javax.swing.JButton();
+        lblRawByte = new javax.swing.JLabel();
+        txtRawByte = new javax.swing.JTextField();
+        btnSend = new javax.swing.JButton();
 
         org.jdesktop.application.ResourceMap resourceMap = org.jdesktop.application.Application.getInstance(plptool.gui.PLPToolApp.class).getContext().getResourceMap(UARTFrame.class);
         setTitle(resourceMap.getString("Form.title")); // NOI18N
@@ -53,6 +57,7 @@ public class UARTFrame extends javax.swing.JInternalFrame {
         txtUART.setColumns(20);
         txtUART.setEditable(false);
         txtUART.setForeground(resourceMap.getColor("txtUART.foreground")); // NOI18N
+        txtUART.setLineWrap(true);
         txtUART.setRows(5);
         txtUART.setDisabledTextColor(resourceMap.getColor("txtUART.disabledTextColor")); // NOI18N
         txtUART.setName("txtUART"); // NOI18N
@@ -63,20 +68,62 @@ public class UARTFrame extends javax.swing.JInternalFrame {
         });
         jScrollPane1.setViewportView(txtUART);
 
+        btnClear.setText(resourceMap.getString("btnClear.text")); // NOI18N
+        btnClear.setName("btnClear"); // NOI18N
+        btnClear.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnClearActionPerformed(evt);
+            }
+        });
+
+        lblRawByte.setText(resourceMap.getString("lblRawByte.text")); // NOI18N
+        lblRawByte.setName("lblRawByte"); // NOI18N
+
+        txtRawByte.setText(resourceMap.getString("txtRawByte.text")); // NOI18N
+        txtRawByte.setName("txtRawByte"); // NOI18N
+        txtRawByte.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                txtRawByteKeyPressed(evt);
+            }
+        });
+
+        btnSend.setText(resourceMap.getString("btnSend.text")); // NOI18N
+        btnSend.setName("btnSend"); // NOI18N
+        btnSend.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSendActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 520, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 520, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(lblRawByte)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtRawByte, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnSend)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 183, Short.MAX_VALUE)
+                        .addComponent(btnClear)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 264, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 251, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnClear)
+                    .addComponent(lblRawByte)
+                    .addComponent(txtRawByte, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnSend))
                 .addContainerGap())
         );
 
@@ -87,9 +134,27 @@ public class UARTFrame extends javax.swing.JInternalFrame {
          u.receivedData(evt.getKeyChar());
     }//GEN-LAST:event_txtUARTKeyTyped
 
+    private void btnClearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnClearActionPerformed
+        txtUART.setText("");
+    }//GEN-LAST:event_btnClearActionPerformed
+
+    private void btnSendActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSendActionPerformed
+        u.receivedData((char) plptool.PLPToolbox.parseNumInt(txtRawByte.getText()));
+    }//GEN-LAST:event_btnSendActionPerformed
+
+    private void txtRawByteKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtRawByteKeyPressed
+        if(evt.getKeyCode() == evt.VK_ENTER) {
+            u.receivedData((char) plptool.PLPToolbox.parseNumInt(txtRawByte.getText()));
+        }
+    }//GEN-LAST:event_txtRawByteKeyPressed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnClear;
+    private javax.swing.JButton btnSend;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lblRawByte;
+    private javax.swing.JTextField txtRawByte;
     private javax.swing.JTextArea txtUART;
     // End of variables declaration//GEN-END:variables
 
