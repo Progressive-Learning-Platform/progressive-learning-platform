cd0c87ad8b8e3c7099411239f4bf643477c88ad7
fritz <fritz@localhost>
2011-03-09 20:44:54 -0600

    libplp math work

--- reference/plp-2.2/sw/libplp/libplp_math.asm
+++ reference/plp-2.2/sw/libplp/libplp_math.asm
@@ -84,5 +84,36 @@ libplp_math_parity:
 	jr $ra
 	andi $v0, $a0, 0x1
 
-#libplp_math_min
-#libplp_math_max
+#
+# libplp_math_min
+#
+# signed min(x,y) function using branches
+#
+libplp_math_min:
+	slt $t0, $a0, $a1
+	beq $t0, $zero, libplp_math_min_y
+	move $v0, $a0
+	j libplp_math_min_done
+	nop
+libplp_math_min_y:
+	move $v0, $a1
+libplp_math_min_done:
+	jr $ra
+	nop
+
+#
+# libplp_math_max
+#
+# signed max(x,y) function using branches
+#
+libplp_math_max:
+	slt $t0, $a0, $a1
+	beq $t0, $zero, libplp_math_max_x
+	move $v0, $a1
+	j libplp_math_max_done
+	nop
+libplp_math_max_x:
+	move $v0, $a0
+libplp_math_max_done:
+	jr $ra
+	nop
