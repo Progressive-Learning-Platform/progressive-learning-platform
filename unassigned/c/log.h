e7c8260b66d7e588c06b03355588b86e0880852e
fritz <fritz@localhost>
2011-08-14 10:51:25 -0500

    added basic logging for cc

---
+++ unassigned/c/log.h
@@ -0,0 +1,12 @@
+#ifndef LOG
+#define LOG
+#include <stdlib.h>
+#include <stdio.h>
+
+#define log(x) _log(1,stdout,x)
+#define err(x) _log(0,stderr,x)
+#define vlog(x) _log(2,stdout,x)
+
+void _log(int l, FILE *f, char* s);
+
+#endif 
