ac2097c8bd4462ea9be70989722c35dd4c6b804b
fritz <fritz@localhost>
2011-09-08 22:14:44 -0500

    cache work, still broken...

---
+++ reference/sw/PLPTool/src/plptool/PLPToolbox.java
@@ -0,0 +1,131 @@
+/*
+    Copyright 2010 David Fritz, Brian Gordon, Wira Mulia
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+package plptool;
+
+import java.awt.event.ActionEvent;
+
+/**
+ *
+ * @author wira
+ */
+public class PLPToolbox {
+    public static long parseNum(String number) {
+        try {
+
+        if(number.startsWith("0x") || number.startsWith("0h")) {
+            number = number.substring(2);
+            return Long.parseLong(number, 16);
+        }
+        else if(number.startsWith("0b")) {
+            number = number.substring(2);
+            return Long.parseLong(number, 2);
+        }
+        else
+            return Long.parseLong(number);
+
+        } catch(Exception e) {
+            Msg.lastError = -1;
+            return Msg.E("parseNum(): Argument is not a valid number\n" + e,
+                            Constants.PLP_NUMBER_ERROR, null);
+        }
+    }
+
+    public static int parseNumInt(String number) {
+        try {
+
+        if(number.startsWith("0x") || number.startsWith("0h")) {
+            number = number.substring(2);
+            return Integer.parseInt(number, 16);
+        }
+        else if(number.startsWith("0b")) {
+            number = number.substring(2);
+            return Integer.parseInt(number, 2);
+        }
+        else
+            return Integer.parseInt(number);
+
+        } catch(Exception e) {
+            Msg.lastError = -1;
+            return Msg.E("parseNum(): Argument is not a valid number\n" + e,
+                            Constants.PLP_NUMBER_ERROR, null);
+        }
+    }
+
+    // Convert 32-bit word to printable ASCII
+    public static String asciiWord(long word) {
+        String tStr = "";
+        long tVal;
+        for(int j = 3; j >= 0; j--) {
+            tVal = word >> (8 * j);
+            tVal &= 0xFF;
+            if(tVal >= 0x21 && tVal <= 0x7E)
+                tStr += (char) tVal + (j == 0 ? "" : " ");
+            else
+                tStr += "." + (j == 0 ? "" : " ");
+        }
+
+        return tStr;
+    }
+
+    public static int getOS(boolean print) {
+        String osIdentifier = System.getProperty("os.name");
+        String osArch = System.getProperty("os.arch");
+
+        if(print) System.out.println(osIdentifier + " " + osArch);
+
+        if(osIdentifier.equals("Linux")) {
+            if(osArch.equals("x86") || osArch.equals("i386"))
+                return Constants.PLP_OS_LINUX_32;
+            else if(osArch.equals("x86_64") || osArch.equals("amd64"))
+                return Constants.PLP_OS_LINUX_64;
+        }
+        else if(osIdentifier.startsWith("Windows")) {
+            if(osArch.equals("x86") || osArch.equals("i386"))
+                return Constants.PLP_OS_WIN_32;
+            else if(osArch.equals("x86_64") || osArch.equals("amd64"))
+                return Constants.PLP_OS_WIN_64;
+        }
+
+        return Constants.PLP_OS_UNKNOWN;
+    }
+
+    public static void attachHideOnEscapeListener(final javax.swing.JFrame frame) {
+        javax.swing.KeyStroke escapeKeyStroke = javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_ESCAPE, 0, false);
+        javax.swing.Action escapeAction = new javax.swing.AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+               frame.setVisible(false);
+            }
+        };
+
+        frame.getRootPane().getInputMap(javax.swing.JComponent.WHEN_IN_FOCUSED_WINDOW).put(escapeKeyStroke, "ESCAPE");
+        frame.getRootPane().getActionMap().put("ESCAPE", escapeAction);
+    }
+
+    public static void attachHideOnEscapeListener(final javax.swing.JDialog frame) {
+        javax.swing.KeyStroke escapeKeyStroke = javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_ESCAPE, 0, false);
+        javax.swing.Action escapeAction = new javax.swing.AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+               frame.setVisible(false);
+            }
+        };
+
+        frame.getRootPane().getInputMap(javax.swing.JComponent.WHEN_IN_FOCUSED_WINDOW).put(escapeKeyStroke, "ESCAPE");
+        frame.getRootPane().getActionMap().put("ESCAPE", escapeAction);
+    }
+}
