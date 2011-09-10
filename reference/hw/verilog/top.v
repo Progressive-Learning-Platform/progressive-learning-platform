dd0f7401517a343a612055af665313685e9c661f
fritz <fritz@localhost>
2011-09-10 11:19:43 -0500

    added debounce circuit to the reset button, much better now. fixes issue 177

--- reference/hw/verilog/top.v
+++ reference/hw/verilog/top.v
@@ -44,12 +44,14 @@ module top(clk,leds,rst,txd,rxd,switches,sseg_an,sseg_display, mod_sram_clk, mod
 	wire [31:0] daddr, dout, din, iaddr, iin;
 	wire [1:0] drw;
 	wire cpu_stall;
+	wire rst_debounced;
 
 	wire int, int_ack;
 
+	debounce(clk, rst, rst_debounced);
 	clock c_t(clk, c);
-	cpu cpu_t(rst, c, cpu_stall, daddr, dout, din, drw, iaddr, iin, int, int_ack);
-        arbiter arbiter_t(rst, c, daddr, dout, din, drw, iaddr, iin, int, int_ack, leds, txd, rxd, switches, sseg_an, sseg_display, cpu_stall, mod_sram_clk, mod_sram_adv, mod_sram_cre, mod_sram_ce, mod_sram_oe, mod_sram_we, mod_sram_lb, mod_sram_ub, mod_sram_data, mod_sram_addr, mod_vga_rgb, mod_vga_hs, mod_vga_vs, mod_gpio_gpio, i_button);
+	cpu cpu_t(rst_debounced, c, cpu_stall, daddr, dout, din, drw, iaddr, iin, int, int_ack);
+        arbiter arbiter_t(rst_debounced, c, daddr, dout, din, drw, iaddr, iin, int, int_ack, leds, txd, rxd, switches, sseg_an, sseg_display, cpu_stall, mod_sram_clk, mod_sram_adv, mod_sram_cre, mod_sram_ce, mod_sram_oe, mod_sram_we, mod_sram_lb, mod_sram_ub, mod_sram_data, mod_sram_addr, mod_vga_rgb, mod_vga_hs, mod_vga_vs, mod_gpio_gpio, i_button);
 endmodule
 	
 
