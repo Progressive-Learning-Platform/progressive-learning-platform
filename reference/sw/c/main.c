7189c5b0d52f513fb99c87577f2bcf7ffdbd4019
fritz <fritz@localhost>
2011-09-21 11:06:08 -0500

    c: symbol table and parse stack output args, getopt improvements

--- reference/sw/c/main.c
+++ reference/sw/c/main.c
@@ -13,73 +13,98 @@ int LOG_LEVEL = 0;
 
 static char *S_FILE_INPUT = NULL;
 static char *S_FILE_OUTPUT = NULL;
+static char *S_PARSE_OUTPUT = NULL;
+static char *S_SYMBOL_OUTPUT = NULL;
 static FILE *FILE_INPUT = NULL;
 static FILE *FILE_OUTPUT = NULL;
+static FILE *PARSE_OUTPUT = NULL;
+static FILE *SYMBOL_OUTPUT = NULL;
 
 stack *parse_stack = NULL;
 symbol_table *sym = NULL;
+
+void print_usage(void) {
+	printf("pcc - plp c compiler\n\n");
+	printf("usage: pcc <options> <input file(s)>\n");
+	printf("options:\n");
+	printf("-o <filename>	output filename\n");
+	printf("-d [0,1,2]	debug level (0=off (default), 1=on, 2=verbose)\n");
+	printf("-s		print symbol table to <output name>.symbol\n");
+	printf("-p		print parse stack to <output name>.parse\n");
+}
 
 void handle_opts(int argc, char *argv[]) {
 	char *dvalue = NULL;
 	char *ivalue = NULL;
 	char *ovalue = NULL;
 
+	int pparse = 0;
+	int psymbol = 0;
+
 	int c;
 
 	opterr = 0;
 	
-	while ((c = getopt(argc, argv, "d:i:o:")) != -1)
+	while ((c = getopt(argc, argv, "d:o:sp")) != -1)
 		switch (c) {
 			case 'd':
 				dvalue = optarg;
 				break;
-			case 'i':
-				ivalue = optarg;
-				break;
 			case 'o':
 				ovalue = optarg;
 				break;
-			case '?':
-				if (optopt == 'd' ||
-				    optopt == 'i' ||
-				    optopt == 'o' )
-					printf("[e] option -%c requires an argument.\n", optopt);
-				else if (isprint (optopt))
-					printf("[e] unknown option '-%c'.\n", optopt);
-				else
-					printf("[e] unknown option character '\\x%x'.\n", optopt);
-					exit(1);
-				default:
-					exit(1);
+			case 's':
+				psymbol = 1;
+				break;
+			case 'p':
+				pparse = 1;
+				break;
+			default:
+				print_usage();
+				exit(1);
 		}
 
 	if (dvalue != NULL) {
 		LOG_LEVEL = atoi(dvalue);
-		vlog("[pcc] setting log level\n");
+		vlog("[pcc] setting log level: %d\n", LOG_LEVEL);
+	}
+
+	/* grab remaining values as inputs */
+	if (optind < argc) {
+		while (optind < argc) {
+			ivalue = argv[optind];
+			log("[pcc] input file: %s\n", argv[optind]);
+			optind++;
+		}
 	}
 
 	if (ivalue != NULL) {
 		S_FILE_INPUT = ivalue;
-		log("[pcc] input file: ");
-		log(S_FILE_INPUT);
-		log("\n");
 	} else {
-		err("[pcc] no input file specified, use -i <input file>\n");
+		print_usage();
 		exit(1);
 	}
 
 	if (ovalue != NULL) {
 		S_FILE_OUTPUT = ovalue;
-		log("[pcc] output file: ");
-		log(S_FILE_OUTPUT);
-		log("\n");
 	} else {
 		S_FILE_OUTPUT = malloc(9);
 		sprintf(S_FILE_OUTPUT,"pcc.out");
-		log("[pcc] output file: ");
-		log(S_FILE_OUTPUT);
-		log("\n");
 	}
+	log("[pcc] output file: %s\n", S_FILE_OUTPUT);
+
+	if (psymbol) {
+		S_SYMBOL_OUTPUT = malloc(sizeof(char) * (strlen(S_FILE_OUTPUT) + 8));
+		sprintf(S_SYMBOL_OUTPUT, "%s.symbol", S_FILE_OUTPUT);
+		log("[pcc] symbol table output: %s\n", S_SYMBOL_OUTPUT);
+	}
+
+	if (pparse) {
+		S_PARSE_OUTPUT = malloc(sizeof(char) * (strlen(S_FILE_OUTPUT) + 7));
+		sprintf(S_PARSE_OUTPUT, "%s.parse", S_FILE_OUTPUT);
+		log("[pcc] parse table output: %s\n", S_PARSE_OUTPUT);
+	}
+	
 }
 
 int main(int argc, char *argv[]) {
@@ -91,18 +116,34 @@ int main(int argc, char *argv[]) {
 	vlog("[pcc] opening files\n");
 	FILE_INPUT = fopen(S_FILE_INPUT,"r");
 	if (FILE_INPUT == NULL) {
-		err("[pcc] cannot open input file!\n");
+		err("[pcc] cannot open input file: %s\n", S_FILE_INPUT);
 		exit(1);
 	}
 	FILE_OUTPUT = fopen(S_FILE_OUTPUT,"w");
 	if (FILE_OUTPUT == NULL) {
-		err("[pcc] cannot open output file!\n");
+		err("[pcc] cannot open output file: %s\n", S_FILE_OUTPUT);
 		exit(1);
 	}
 	yyset_in(FILE_INPUT);
 
+	if (S_SYMBOL_OUTPUT != NULL) {
+		SYMBOL_OUTPUT = fopen(S_SYMBOL_OUTPUT, "w");
+		if (SYMBOL_OUTPUT == NULL) {
+			err("[pcc] cannot open symbol table output file: %s\n", S_SYMBOL_OUTPUT);
+			exit(1);
+		}
+	}
+
+	if (S_PARSE_OUTPUT != NULL) {
+		PARSE_OUTPUT = fopen(S_PARSE_OUTPUT, "w");
+		if (PARSE_OUTPUT == NULL) {
+			err("[pcc] cannot open parse table output file: %s\n", S_PARSE_OUTPUT);
+			exit(1);
+		}
+	}
+
 	/* create an empty symbol table */
-	sym = new_symbol_table(NULL);
+//	sym = new_symbol_table(NULL);
 
 	log("[pcc] starting frontend\n");
 	yyparse();
