3306561fe679e7ac3a7426a2d13fdc0b6d7cd0f2
fritz <fritz@localhost>
2012-01-16 10:09:29 -0600

    updated buildbot and nightly scripts

--- misc/buildbot/nightly_backup.bash
+++ misc/buildbot/nightly_backup.bash
@@ -7,8 +7,11 @@ cd /backup/fritz/tmp
 #google code
 hg clone https://progressive-learning-platform.googlecode.com/hg/ plp-reference-code
 hg clone https://wiki.progressive-learning-platform.googlecode.com/hg plp-reference-wiki
-#hg clone https://okstate-ecen4243-plp-spring-2011.googlecode.com/hg/ plp-sp2011-code
-#hg clone https://wiki.okstate-ecen4243-plp-spring-2011.googlecode.com/hg/ plp-sp2011-wiki
+hg clone https://okstate-ecen4243-plp-spring-2012.googlecode.com/hg/ ecen4243-plp-sp2012-code
+hg clone https://wiki.okstate-ecen4243-plp-spring-2012.googlecode.com/hg/ ecen4243-plp-sp2012-wiki
+hg clone https://okstate-ensc3213-plp-spring-2012.googlecode.com/hg/ ensc3213-plp-sp2012-code
+hg clone https://wiki.okstate-ensc3213-plp-spring-2012.googlecode.com/hg/ ensc3213-plp-sp2012-wiki
+
 
 #ROME data
 mkdir website
