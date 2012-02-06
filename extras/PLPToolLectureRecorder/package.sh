ddbaa3a2ef35247d99bc134e6b63b6fe351897e6
wira <wira@Tiberius>
2012-02-06 11:52:49 -0600

    plptool installer update

--- extras/PLPToolLectureRecorder/package.sh
+++ extras/PLPToolLectureRecorder/package.sh
@@ -1,4 +1,4 @@
 javac -cp ../../reference/sw/PLPTool/dist/PLPTool.jar LectureRecorder.java
 zip LectureRecorder.jar *.class
-scp LectureRecorder.jar rome.ceat.okstate.edu:/home/fritz/plp/goodies
+#scp LectureRecorder.jar rome.ceat.okstate.edu:/home/fritz/plp/goodies
 rm *.class
