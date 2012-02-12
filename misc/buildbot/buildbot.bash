f9312306f7784852e54832cd689eeffdefd481af
wira <wira@Nero>
2012-02-12 10:59:19 -0600

    buildbot updatE

--- misc/buildbot/buildbot.bash
+++ misc/buildbot/buildbot.bash
@@ -2,21 +2,29 @@
 
 DATE=`date +%y%m%d`
 
-# we assume that /backup/fritz/buildbot has a valid hg repo in it
+# assume we're in misc/buildbot and make a build repo in ../../../buildbot_tmp
 # we also assume that NOBODY makes changes in the buildbot directory, i.e. it's always sane
 
 #update the repo
-cd /backup/fritz/buildbot
+echo ===============================================================================
+echo - Making sure the repo is up to date!
+echo ===============================================================================
 hg pull
 hg update
 
 #copy the repo for building
-cd /backup/fritz
-cp -R /backup/fritz/buildbot /backup/fritz/buildbot_tmp
-cd /backup/fritz/buildbot_tmp
+BBOTPATH=`pwd`
+cd ../../
+mkdir ../buildbot_tmp
+cp -R * ../buildbot_tmp 
+cd ../buildbot_tmp
+BUILDROOT=`pwd`
 
 #we're in our temporary repo, let's build some things
-cd reference/hw
+echo ===============================================================================
+echo - Building hardware...
+echo ===============================================================================
+cd $BUILDROOT/reference/hw
 source /opt/Xilinx/13.1/ISE_DS/settings64.sh
 cd bootrom
 sh build.sh > bootrom_log
@@ -26,28 +34,41 @@ sh scripts/build_1200k_linux.sh > build_nexys2_1200k_log
 sh scripts/build_nexys3_linux.sh > build_nexys3_log
 
 #update the PLPTool nightly version
-cd ../sw/PLPTool/src/plptool
+echo ===============================================================================
+echo - Building PLPTool...
+echo ===============================================================================
+echo   - Updating Version string...
+cd $BUILDROOT/reference/sw/PLPTool/src/plptool
 sed -i "s/\"\(.*\)\"/\"\1-${DATE}\"/g" Version.java 
 
 #build PLPTool
+echo   - Building binary...
 cd ../..
 ant clean
 ant package-for-store > build_plptool_log
+echo   - Building javadoc...
 ant javadoc > build_javadoc_log
 
 #build the classroom module
-cd /backup/fritz/buildbot_tmp/extras/PLPToolLectureRecorder
+echo   - BUilding lecture recorder...
+cd $BUILDROOT/extras/PLPToolLectureRecorder
 ./package.sh > build_lecturerecorder_log
 
 #run the autotest
-cd /backup/fritz/buildbot_tmp/reference/sw
+echo ===============================================================================
+echo - Commencing PLP auto-test...
+echo ===============================================================================
+cd $BUILDROOT/reference/sw 
 yes | /usr/local/bin/djtgcfg prog -d Nexys2 -i 0 -f ../hw/build/build_500k_volatile/top.bit >> auto_test_log
 sleep 10
 java -jar PLPTool/store/PLPToolStatic.jar -plp examples/auto_test.plp -p /dev/ttyUSB0 >> auto_test_log
 python /backup/fritz/scripts/auto_test.py /dev/ttyUSB0 >> auto_test_log
 
 #everything should be built (or have failed), so get things ready to push to the website
-cd /backup/fritz/buildbot_tmp
+echo ===============================================================================
+echo - Pushing...
+echo ===============================================================================
+cd $BUILDROOT 
 mkdir to_push
 cp reference/sw/auto_test_log to_push
 cp reference/hw/*_log to_push
@@ -59,23 +80,34 @@ cp reference/hw/build/build_1200k_volatile/top.mcs to_push/nexys2_1200k.mcs
 cp reference/hw/build/build_nexys3_volatile/top.bit to_push/nexys3.bit
 
 #build the Windows installer
-cd /backup/fritz/buildbot_tmp/extras/PLPToolInstaller
-mkdir Hardware
-cp ../../to_push/*.bit ./Hardware
-cp ../../to_push/*.mcs ./Hardware
+echo ===============================================================================
+echo   - Building PLP Tool Windows installer...
+echo ===============================================================================
+cd $BUILDROOT/extras/PLPToolInstaller
+mkdir hardware
+cp ../../to_push/*.bit ./hardware
+cp ../../to_push/*.mcs ./hardware
 ./build_linuxhost.sh > build_plptoolinstall_log
+cp build_plptoolinstall_log $BUILDROOT/to_push
 
+echo ===============================================================================
+echo   - Pushing sw...
+echo ===============================================================================
+cd $BUILDROOT
 cp reference/sw/PLPTool/*_log to_push
 cp reference/sw/PLPTool/store/* to_push
 cp extras/PLPToolInstaller/plptoolsetup.exe to_push
 cp -R reference/sw/PLPTool/dist/javadoc to_push
 
 #our data is ready to send off to the webserver
+echo ===============================================================================
+tar -czvf nightly-$DATE.tar.gz to_push/
+mv nightly-$DATE.tar.gz $BBOPTPATH/
 ssh fritz@rome.ceat.okstate.edu "rm -rf plp/nightly/*"
 scp -r to_push/* fritz@rome.ceat.okstate.edu:~/plp/nightly
 
 #nuke the buildbot tmp
-cd /backup/fritz
-rm -rf buildbot_tmp
+cd $BBOPTPATH
+rm -rf $BUILDROOT
 
 
