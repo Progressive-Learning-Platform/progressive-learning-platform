25597436d577f6ea528d26009e23d9796d581a18
wira <wira@wintermute>
2012-08-31 16:57:39 -0500

    plptool autotest reorg

---
+++ reference/sw/PLPTool/autotests/Hazards.java
@@ -0,0 +1,26 @@
+import plptool.*;
+import plptool.testsuite.*;
+import plptool.gui.ProjectDriver;
+
+public class Hazards implements Tester {
+	public void configure(java.awt.Robot r) { }
+	
+	public void run(ProjectDriver plp) {
+		plp.open("../examples/chutes_and_hazards.plp", false);
+		
+		if(plp.assemble() != Constants.PLP_OK)		fail();
+		if(!plp.isAssembled()) 						fail();
+		if(plp.simulate() != Constants.PLP_OK) 		fail();
+		
+		for(int i = 0; i < 5000; i++)
+			plp.sim.step();
+		long out = (Long) plp.sim.bus.read(0xf0200000L);
+		if(out == 0xffL)
+			pass();
+		else
+			fail();
+	}
+	
+	private void fail() { AutoTest.p("FAIL"); System.exit(-1); }
+	private void pass() { AutoTest.p("PASS"); System.exit(0); }
+}
--- reference/sw/PLPTool/testcase/jump_test.asm
+++
@@ -1,7 +0,0 @@
-.org 0x70000000
-start:
-nop
-nop
-nop
-j start
-nop
