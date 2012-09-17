4ea034bbd2406a4f20078c865cd1c79021d65195
wira <wira@wintermute>
2012-09-17 17:32:20 -0500

    plptool hc11 module update

--- extras/PLP6811ISA/src/plptool/hc11/Asm.java
+++ extras/PLP6811ISA/src/plptool/hc11/Asm.java
@@ -1,6 +1,19 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+    Copyright 2012 David Fritz, Brian Gordon, Wira Mulia
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
  */
 
 package plptool.hc11;
@@ -23,20 +36,25 @@ public class Asm extends PLPAsm {
     }
 
     public int assemble() {
-        StringBuilder str = new StringBuilder();
-        for(int i = 0; i < sourceList.size(); i++)
-            str.append(sourceList.get(i).getAsmString());
-        lines = str.toString().split("\\r?\\n");
+        
         setAssembled(true);
         return Constants.PLP_OK;
     }
 
     public int preprocess(int index) {
-        /* In first pass, we populate our symbol table and map addresses to
-         * the correspding source file and line number. For this, we also need
+        PC = 0x0000;
+
+        /* In the first pass, we populate our symbol table and map addresses to
+         * the corresponding source file and line number. For this, we also need
          * to figure out instruction lengths (damn variable length instructions!)
          */
+        for(int i = 0; i < sourceList.size(); i++) {
+            lines = sourceList.get(i).getAsmString().split("\\r?\\n");
 
+            for(int l = 0; l < lines.length; l++) {
+                
+            }
+        }
 
         return Constants.PLP_OK;
     }
