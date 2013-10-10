17d4dfcdfb7a55ae3d6ac1cdc5e093e3bb442c1f
wira <wira@alaei>
2013-10-10 09:45:02 -0500

    removed binaries from the repo

--- reference/sw/PLPTool-Distrib/clean.sh
+++ reference/sw/PLPTool-Distrib/clean.sh
@@ -2,5 +2,8 @@
 rm -rf ../PLPTool/build
 rm -rf ../PLPTool/dist
 rm -rf ../PLPTool-Extensions/AutoSaver5/build
+rm -rf ../PLPTool-Extensions/AutoSaver5/dist
 rm -rf ../PLPTool-Extensions/CacheSim/build
+rm -rf ../PLPTool-Extensions/CacheSim/dist
 rm -rf ../PLPTool-Extensions/Flowchart/build
+rm -rf ../PLPTool-Extensions/Flowchart/dist
