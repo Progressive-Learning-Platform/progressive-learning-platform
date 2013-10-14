261cf4b909d706674f735ad1cda4ecdb355ec166
wira <wira@wintermute>
2013-10-14 12:11:44 -0500

    plptool distrib clean script update

--- reference/sw/PLPTool-Distrib/clean.sh
+++ reference/sw/PLPTool-Distrib/clean.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+rm -f *.log
+rm -f *.deb
+rm -rf deb/usr
+rm -f *.exe
 rm -rf ../PLPTool/build
 rm -rf ../PLPTool/dist
+rm -rf ../PLPTool/store
+rm -rf ../PLPTool/modtools/*.class
+rm -rf ../PLPTool/autotests/*.class
 rm -rf ../PLPTool-Extensions/AutoSaver5/build
 rm -rf ../PLPTool-Extensions/AutoSaver5/dist
 rm -rf ../PLPTool-Extensions/CacheSim/build
