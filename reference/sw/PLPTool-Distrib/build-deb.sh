a4cb8c97fcfece14f0b2e0b510c74ca03c5a1f09
wira <wira@wintermute>
2013-12-13 15:05:19 -0600

    plptool build deb script update

--- reference/sw/PLPTool-Distrib/build-deb.sh
+++ reference/sw/PLPTool-Distrib/build-deb.sh
@@ -15,6 +15,7 @@ EXT_PREFIX="../PLPTool-Extensions"
 EXT_FLOWCHART="$EXT_PREFIX/Flowchart/dist/Flowchart.jar"
 EXT_CACHESIM="$EXT_PREFIX/CacheSim/dist/CacheSim.jar"
 EXT_AUTOSAVER5="$EXT_PREFIX/AutoSaver5/dist/AutoSaver5.jar"
+EXT_SOCKETINTERFACE="$EXT_PREFIX/SocketInterface/dist/SocketInterface.jar"
 EXT_DEST="./deb/usr/lib/plptool5/extensions"
 
 if [ ! -e ../PLPTool/store/PLPToolStatic.jar ]
@@ -48,6 +49,10 @@ then
   if [ -e $EXT_AUTOSAVER5 ]
   then
     cp -v $EXT_AUTOSAVER5 $EXT_DEST
+  fi
+  if [ -e $EXT_SOCKETINTERFACE ]
+  then
+    cp -v $EXT_SOCKETINTERFACE $EXT_DEST
   fi
   cp -vr ../examples ./deb/usr/share/plptool5
   cp -vr ../libplp ./deb/usr/share/plptool5
