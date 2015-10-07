385f9a10a6f9a45e997b1720fadcb54c7a208994
Morgan Nesbitt <nmorgan1214@gmail.com>
2015-10-06 23:23:36 -0700

    Make Assert Equals dynamic + parsing int

--- reference/sw/PLPTool/autotests/junit/PLPToolboxTest.java
+++ reference/sw/PLPTool/autotests/junit/PLPToolboxTest.java
@@ -11,6 +11,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import plptool.PLPAsm;
 import plptool.PLPToolbox;
 
 public class PLPToolboxTest
@@ -19,11 +20,6 @@ public class PLPToolboxTest
 	private static String numHexWithXSuffix;
 	private static String numHexWithHSuffix;
 	private static String numBinaryWithBSuffix;
-	
-	private static long numIntExpected;
-	private static long numHexWithXSuffixExpected;
-	private static long numHexWithHSuffixExpected;
-	private static long numBinaryWithBSuffixExpected;
 	
 	private static ByteArrayOutputStream outStream = new ByteArrayOutputStream();
 	
@@ -34,11 +30,6 @@ public class PLPToolboxTest
 		numHexWithXSuffix = "0xF";
 		numHexWithHSuffix = "0hF";
 		numBinaryWithBSuffix = "0b101";
-		
-		numIntExpected = 12;
-		numHexWithXSuffixExpected = 15;
-		numHexWithHSuffixExpected = 15;
-		numBinaryWithBSuffixExpected = 5;
 	}
 	
 	@Before
@@ -54,16 +45,25 @@ public class PLPToolboxTest
 		System.setErr(null);
 	}
 	
+	// parseNum
 	@Test
 	public void parseNumHexOrBinaryIntoUnsignedTest()
 	{
-		assertEquals("parseNum 12 == 12", numIntExpected,
-				PLPToolbox.parseNum(numInt));
-		assertEquals("parseNum 0xF == 15", numHexWithXSuffixExpected,
+		long numIntExpected = 12;
+		long numHexWithXSuffixExpected = 15;
+		long numHexWithHSuffixExpected = 15;
+		long numBinaryWithBSuffixExpected = 5;
+		
+		assertEquals("parseNum " + numInt + " == " + numIntExpected,
+				numIntExpected, PLPToolbox.parseNum(numInt));
+		assertEquals("parseNum " + numHexWithXSuffix + " == "
+				+ numHexWithXSuffixExpected, numHexWithXSuffixExpected,
 				PLPToolbox.parseNum(numHexWithXSuffix));
-		assertEquals("parseNum 0hF == 15", numHexWithHSuffixExpected,
+		assertEquals("parseNum " + numHexWithHSuffix + " == "
+				+ numHexWithHSuffixExpected, numHexWithHSuffixExpected,
 				PLPToolbox.parseNum(numHexWithHSuffix));
-		assertEquals("parseNum 0b101 == 5", numBinaryWithBSuffixExpected,
+		assertEquals("parseNum " + numBinaryWithBSuffix + " == "
+				+ numBinaryWithBSuffixExpected, numBinaryWithBSuffixExpected,
 				PLPToolbox.parseNum(numBinaryWithBSuffix));
 		
 		assertEquals("parseNum 0b000000000 == 0", 0,
@@ -80,12 +80,10 @@ public class PLPToolboxTest
 		assertEquals("parseNum returns -1 on 0Xf", -1,
 				PLPToolbox.parseNum("0Xf"));
 		
-		
 		assertEquals(
 				"parseNum stderr is [ERROR] Number error: '0Xf' is not a valid number",
-				"[ERROR] Number error: '0Xf' is not a valid number".trim(), 
+				"[ERROR] Number error: '0Xf' is not a valid number".trim(),
 				outStream.toString().trim());
-		
 		
 		outStream.reset();
 		
@@ -94,15 +92,71 @@ public class PLPToolboxTest
 		
 		assertEquals(
 				"parseNum stderr is [ERROR] Number error: '0Hf' is not a valid number",
-				"[ERROR] Number error: '0Hf' is not a valid number".trim(), 
+				"[ERROR] Number error: '0Hf' is not a valid number".trim(),
 				outStream.toString().trim());
 		
 	}
 	
+	// parseNumSilent
 	@Test
 	public void parseNumSilentHexOrBinaryIntoUnsignedTest()
 	{
+		parseNumHexOrBinaryIntoUnsignedTest();
+	}
+	
+	// parseNumInt
+	@Test
+	public void parseNumIntHexOrBinaryIntoUnsignedTest()
+	{
+		int numIntExpected = 12;
+		int numHexWithXSuffixExpected = 15;
+		int numHexWithHSuffixExpected = 15;
+		int numBinaryWithBSuffixExpected = 5;
 		
+		assertEquals("parseNum " + numInt + " == " + numIntExpected,
+				numIntExpected, PLPToolbox.parseNum(numInt));
+		assertEquals("parseNum " + numHexWithXSuffix + " == "
+				+ numHexWithXSuffixExpected, numHexWithXSuffixExpected,
+				PLPToolbox.parseNum(numHexWithXSuffix));
+		assertEquals("parseNum " + numHexWithHSuffix + " == "
+				+ numHexWithHSuffixExpected, numHexWithHSuffixExpected,
+				PLPToolbox.parseNum(numHexWithHSuffix));
+		assertEquals("parseNum " + numBinaryWithBSuffix + " == "
+				+ numBinaryWithBSuffixExpected, numBinaryWithBSuffixExpected,
+				PLPToolbox.parseNum(numBinaryWithBSuffix));
+		
+		assertEquals("parseNum 0b000000000 == 0", 0,
+				PLPToolbox.parseNum("0b000000000"));
+		
+		assertEquals("Parsing 0xf = 15", 15, PLPToolbox.parseNum("0xf"));
+		
+		assertEquals("parseNum -5 == -5", -5, PLPToolbox.parseNum("-5"));
+	}
+	
+	// tryResolveLabel
+	@Test
+	public void tryResolveLabelResolveOrParseAddress()
+	{
+		String label = "";
+		PLPAsm asm = null;
+		// fail("Not implemented");
+	}
+	
+	// format32Hex
+	// Function says format a long as 8 digit hex in String prefixed with
+	// '0x'
+	@Test
+	public void format32Hex8DigitsWithPrefix0X()
+	{
+		long intMaxLength = Integer.MAX_VALUE;
+		long longMaxLength = Long.MAX_VALUE;
+		
+		// 0x is length 2 + 8 digit hex
+		assertEquals("Formatted", 10, PLPToolbox.format32Hex(intMaxLength)
+				.length());
+		
+		assertEquals("Formatted", 10, PLPToolbox.format32Hex(longMaxLength)
+				.length());
 	}
 	
 }
