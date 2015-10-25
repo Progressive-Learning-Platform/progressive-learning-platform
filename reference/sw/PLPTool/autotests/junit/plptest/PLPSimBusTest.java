e59f9ec120c577ae295dfc7bdd6f72e5cd953261
Fatimah <falowa@asu.edu>
2015-10-24 22:33:43 -0700

    test add method

--- reference/sw/PLPTool/autotests/junit/plptest/PLPSimBusTest.java
+++ reference/sw/PLPTool/autotests/junit/plptest/PLPSimBusTest.java
@@ -1,13 +1,33 @@
+package junit.plptest
 
+import org.junit.Before;
+import org.junit.Test;
+import plptool.Constants;
+import plptool.PLPSimBus;
+import plptool.mods.MemModule;
+
+import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.*;
 
-import org.junit.Test;
 
 public class PLPSimBusTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    //Creating a word Aligned memModule
+    private MemModule memModule;
+    private PLPSimBus plpSimBus;
+    
+    @Before
+    public void setup(){
+        
+        memModule = new MemModule(100L, 200, true);
+        plpSimBus = new PLPSimBus();
+    }
+    
+    @Test
+    public void testAdd(){
+        int index = plpSimBus.add(memModule);
+        assertEquals(index,0);
+        assertNotNull(index);
+    }
 
 }
