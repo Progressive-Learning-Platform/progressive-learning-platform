eebf988ea81c5f5b3aa9fb507b7d5d5ffb89d35a
Morgan Nesbitt <nmorgan1214@gmail.com>
2015-10-25 21:04:47 -0700

    Add Project Driver Test to suite

--- reference/sw/PLPTool/autotests/junit/PLPTestSuite.java
+++ reference/sw/PLPTool/autotests/junit/PLPTestSuite.java
@@ -1,19 +1,22 @@
 package junit;
 
 import junit.plp.core.PLPASMSourceTest;
+import junit.plp.core.projectdriver.ProjectDriverTest;
 import junit.plpToolBox.PLPToolBoxSystemTesting;
 import junit.plpToolBox.PLPToolboxParseTesting;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+//@formatter:off
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-  PLPToolboxParseTesting.class,
-  PLPToolBoxSystemTesting.class,
-  PLPASMSourceTest.class
+	ProjectDriverTest.class,
+	PLPToolboxParseTesting.class,
+	PLPToolBoxSystemTesting.class,
+	PLPASMSourceTest.class
 })
-
+//@formatter:on
 public class PLPTestSuite
 {
 	
