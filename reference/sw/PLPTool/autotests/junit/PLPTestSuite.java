f4f609500e2d605bbdae8fc8527072b05031257e
Morgan Nesbitt <nmorgan1214@gmail.com>
2015-11-01 15:47:31 -0700

    Add arch registry tests to suite

--- reference/sw/PLPTool/autotests/junit/PLPTestSuite.java
+++ reference/sw/PLPTool/autotests/junit/PLPTestSuite.java
@@ -1,6 +1,8 @@
 package junit;
 
 import junit.plp.core.PLPASMSourceTest;
+import junit.plp.core.archregistry.ArchRegistryGetArchitecture;
+import junit.plp.core.archregistry.ArchRegistryGets;
 import junit.plp.core.projectdriver.ProjectDriverAlterProjectTest;
 import junit.plp.core.projectdriver.ProjectDriverOpenTest;
 import junit.plp.core.projectdriver.ProjectDriverSaveTest;
@@ -13,6 +15,8 @@ import org.junit.runners.Suite;
 //@formatter:off
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+	ArchRegistryGets.class,
+	ArchRegistryGetArchitecture.class,
 	ProjectDriverAlterProjectTest.class,
 	ProjectDriverSaveTest.class,
 	ProjectDriverOpenTest.class,
