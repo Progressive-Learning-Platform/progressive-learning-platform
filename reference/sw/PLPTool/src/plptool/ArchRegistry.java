bd55a5bd3ac09e20b7f8511e7fcdea9ecb7c6f30
Zach Moore <zcmoore@asu.edu>
2015-11-01 18:43:51 -0700

    Remove unused imports

--- reference/sw/PLPTool/src/plptool/ArchRegistry.java
+++ reference/sw/PLPTool/src/plptool/ArchRegistry.java
@@ -22,7 +22,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import plptool.gui.ProjectDriver;
 import plptool.mips.Architecture;
