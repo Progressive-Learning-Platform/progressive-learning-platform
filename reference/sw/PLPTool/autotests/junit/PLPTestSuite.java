5dad5d30afd00f1d85694e1700e2d141bcb66e1f
Morgan Nesbitt <nmorgan1214@gmail.com>
2015-11-05 14:37:42 -0700

    Add additional tests to suite

--- reference/sw/PLPTool/autotests/junit/PLPTestSuite.java
+++ reference/sw/PLPTool/autotests/junit/PLPTestSuite.java
@@ -1,13 +1,18 @@
 package junit;
 
+import junit.plp.architecture.registry.RegisterArchitectureTest;
 import junit.plp.core.PLPASMSourceTest;
+import junit.plp.core.PLPSimBusCoreTest;
 import junit.plp.core.archregistry.ArchRegistryGetArchitecture;
 import junit.plp.core.archregistry.ArchRegistryGets;
+import junit.plp.core.modules.PLPSimBusModuleTest;
 import junit.plp.core.projectdriver.ProjectDriverAlterProjectTest;
 import junit.plp.core.projectdriver.ProjectDriverOpenTest;
 import junit.plp.core.projectdriver.ProjectDriverSaveTest;
 import junit.plpToolBox.PLPToolBoxSystemTesting;
 import junit.plpToolBox.PLPToolboxParseTesting;
+import junit.plptest.ArchRegistryTest;
+import junit.plptest.PLPSimBusTest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -15,8 +20,13 @@ import org.junit.runners.Suite;
 //@formatter:off
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+	PLPSimBusTest.class,
+	PLPSimBusModuleTest.class,
+	PLPSimBusCoreTest.class,
 	ArchRegistryGets.class,
 	ArchRegistryGetArchitecture.class,
+	ArchRegistryTest.class,
+	RegisterArchitectureTest.class,
 	ProjectDriverAlterProjectTest.class,
 	ProjectDriverSaveTest.class,
 	ProjectDriverOpenTest.class,
