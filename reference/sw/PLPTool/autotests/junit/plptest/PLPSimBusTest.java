ca7d0ba61e3098701cda7228aec17f8034354e34
Morgan Nesbitt <nmorgan1214@gmail.com>
2015-11-05 20:41:08 -0700

    Test SimBus write bounds

--- reference/sw/PLPTool/autotests/junit/plptest/PLPSimBusTest.java
+++ reference/sw/PLPTool/autotests/junit/plptest/PLPSimBusTest.java
@@ -16,6 +16,7 @@ import plptool.PLPSimBus;
 import plptool.PLPSimBusModule;
 import plptool.dmf.Callback;
 import plptool.dmf.CallbackRegistry;
+import plptool.mips.MIPSInstr;
 import plptool.mods.MemModule;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
@@ -348,13 +349,95 @@ public class PLPSimBusTest
 	}
 	
 	@Test
-	public void testWriteOutofbounds()
+	public void testWriteOutOfBoundsOfModule()
+	{
+		//100L and 200
+		plpSimBus.add(memModule);
+		
+		long lowerBound = 99L;
+		long higherBound = 201L;
+		
+		assertNotSame(Constants.PLP_OK, plpSimBus.write(lowerBound, Long.MAX_VALUE, false));
+		assertNotSame(Constants.PLP_OK, plpSimBus.write(higherBound, Long.MAX_VALUE, false));
+		
+		//No module in range, no read
+		assertEquals(-1, plpSimBus.read(lowerBound));
+		assertEquals(-1, plpSimBus.read(higherBound));
+	}
+	
+	@Test
+	public void testWriteAddressBoundsNoModule()
+	{
+		long negativeLongBound = Long.MIN_VALUE;
+		long negativeIntegerLowerBound = Integer.MIN_VALUE;
+		long lowerBound = 0L;
+		long positiveIntegerMaxBound = Integer.MAX_VALUE;
+		long positiveLongBound = Long.MIN_VALUE;
+		
+		assertNotSame(Constants.PLP_OK, plpSimBus.write(negativeLongBound, Long.MAX_VALUE, false));
+		assertNotSame(Constants.PLP_OK, plpSimBus.write(negativeIntegerLowerBound, Long.MAX_VALUE, false));
+		assertNotSame(Constants.PLP_OK, plpSimBus.write(lowerBound, Long.MAX_VALUE, false));
+		assertNotSame(Constants.PLP_OK, plpSimBus.write(positiveIntegerMaxBound, Long.MAX_VALUE, false));
+		assertNotSame(Constants.PLP_OK, plpSimBus.write(positiveLongBound, Long.MAX_VALUE, false));
+	}
+	
+	@Test
+	public void testWriteAddressBoundsWithModuleNoOverlap()
 	{
 		plpSimBus.add(memModule);
-		plpSimBus.write(20L, 1234L, true);
+		long negativeLongBound = Long.MIN_VALUE;
+		long negativeIntegerLowerBound = Integer.MIN_VALUE;
+		long lowerBound = 0L;
+		long positiveIntegerMaxBound = Integer.MAX_VALUE;
+		long positiveLongBound = Long.MIN_VALUE;
 		
-		assertEquals(0L, plpSimBus.read(120L));
-		assertNull(plpSimBus.read(20L));
+		assertNotSame(Constants.PLP_OK, plpSimBus.write(negativeLongBound, Long.MAX_VALUE, false));
+		assertNotSame(Constants.PLP_OK, plpSimBus.write(negativeIntegerLowerBound, Long.MAX_VALUE, false));
+		assertNotSame(Constants.PLP_OK, plpSimBus.write(lowerBound, Long.MAX_VALUE, false));
+		assertNotSame(Constants.PLP_OK, plpSimBus.write(positiveIntegerMaxBound, Long.MAX_VALUE, false));
+		assertNotSame(Constants.PLP_OK, plpSimBus.write(positiveLongBound, Long.MAX_VALUE, false));
+	}
+	
+	@Test
+	public void testWriteToUnEnabledModuleInRange()
+	{
+		int addedIndex = plpSimBus.add(memModule);
+		plpSimBus.disableMod(addedIndex);
+		
+		//100L to 200
+		long inclusiveLowerBound = 100L;
+		long inclusiveUpperBound = 200L;
+		long mediumBound = 150L;
+		
+		assertNotSame(Constants.PLP_OK, plpSimBus.write(inclusiveLowerBound, Long.MAX_VALUE, false));
+		assertNotSame(Constants.PLP_OK, plpSimBus.write(mediumBound, Long.MAX_VALUE, false));
+		assertNotSame(Constants.PLP_OK, plpSimBus.write(inclusiveUpperBound, Long.MAX_VALUE, false));
+		
+		//Disabled but should still allow read, initial values
+		assertEquals(0L, plpSimBus.read(inclusiveUpperBound));
+		assertEquals(0L, plpSimBus.read(mediumBound));
+		assertEquals(0L, plpSimBus.read(inclusiveUpperBound));
+	}
+	
+	@Test
+	public void testWriteToEnabledModuleInRangeConstantValue()
+	{
+		int addedIndex = plpSimBus.add(memModule);
+		plpSimBus.enableMod(addedIndex);
+		
+		//100L to 200
+		long inclusiveLowerBound = 100L;
+		long inclusiveUpperBound = 200L;
+		long mediumBound = 150L;
+		
+		assertEquals(Constants.PLP_OK, plpSimBus.write(inclusiveLowerBound, Long.MAX_VALUE, false));
+		assertEquals(Constants.PLP_OK, plpSimBus.write(mediumBound, Long.MAX_VALUE, false));
+		assertEquals(Constants.PLP_OK, plpSimBus.write(inclusiveUpperBound, Long.MAX_VALUE, false));
+		
+		//Should read correct value
+		assertEquals(Long.MAX_VALUE, plpSimBus.read(inclusiveUpperBound));
+		assertEquals(Long.MAX_VALUE, plpSimBus.read(mediumBound));
+		assertEquals(Long.MAX_VALUE, plpSimBus.read(inclusiveUpperBound));
 	}
 	
 	@Test
